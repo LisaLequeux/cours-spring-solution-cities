11a5e1455fddc39073daa6e78ab7629f843e319b
LisaLequeux <147843628+LisaLequeux@users.noreply.github.com>
2024-01-08 12:20:13 +0100

    Update CountryRepository.java

--- src/main/java/monprojet/dao/CountryRepository.java
+++ src/main/java/monprojet/dao/CountryRepository.java
@@ -1,5 +1,6 @@
 package monprojet.dao;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,4 +13,17 @@ import monprojet.entity.Country;
 
 public interface CountryRepository extends JpaRepository<Country, Integer> {
 
+    // Méthode 1 à ajouter : renvoyer la population d'un pays à partir de son id
+    @Query(value = "SELECT SUM(population) AS population"
+            +"FROM country INNER JOIN city USING id"
+            +"WHERE country.id = : idPays",
+            nativeQuery = true)
+    public int Integer(int idPays);
+
+    // Méthode 2 à ajouter : renvoyer la liste avec le nom et la population des pays sans paramètres
+    @Query(value = "SELECT country.name, SUM(population) AS population"
+            +"FROM country INNER JOIN city USING id"
+            +"GROUP BY name, country.id",
+            nativeQuery = true)
+    public List<PPP> PopParPays();
 }
